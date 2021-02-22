72212b9583e1cc2f5f0ccbf38f64016538da0231
naserbel <scearnproject@gmail.com>
2021-02-22 20:13:28 +0200

    multiple linea regression 0.1

--- src/main/scala/statistics/multiple_linear_regression.py
+++ src/main/scala/statistics/multiple_linear_regression.py
@@ -7,4 +7,25 @@ def main():
         inv = np.linalg.inv(a)
         return inv.dot(x.transpose().dot(y))
 
+    line = input().split()
+    m = int(line[0])
+    n = int(line[1])
+
+    x = np.zeros(n, m)
+    y = np.zeros(n)
+    for i in range(n):
+        inputLine = input().split()
+        y[i] = float(inputLine[len(inputLine)])
+        for j in range(m):
+            x[i][j] = float(inputLine[j])
+    b = compute_b(x, y)
+    q = int(input())
+    x_test = np.zeros(q, m)
+    for k in range(q):
+        test_line = input().split()
+        for p in range(m):
+            x_test[k][p] = float(test_line[p])
+
+
+
 
