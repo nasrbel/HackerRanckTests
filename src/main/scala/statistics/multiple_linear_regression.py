9414906944c2bb59c2104c68773d84febd763bcc
naserbel <scearnproject@gmail.com>
2021-02-22 20:30:25 +0200

    multiple linea regression (without taking error into consideration)

--- src/main/scala/statistics/multiple_linear_regression.py
+++ src/main/scala/statistics/multiple_linear_regression.py
@@ -7,25 +7,34 @@ def main():
         inv = np.linalg.inv(a)
         return inv.dot(x.transpose().dot(y))
 
-    line = input().split()
+    line = input().split(' ')
     m = int(line[0])
     n = int(line[1])
-
-    x = np.zeros(n, m)
+    x = np.zeros((n, m+1))
     y = np.zeros(n)
     for i in range(n):
-        inputLine = input().split()
-        y[i] = float(inputLine[len(inputLine)])
+        inputLine = input().split(' ')
+        y[i] = float(inputLine[len(inputLine)-1])
+        x[i][0] = 1.0
         for j in range(m):
-            x[i][j] = float(inputLine[j])
+            x[i][j+1] = float(inputLine[j])
     b = compute_b(x, y)
     q = int(input())
-    x_test = np.zeros(q, m)
+    x_test = np.zeros((q, m+1))
     for k in range(q):
-        test_line = input().split()
+        test_line = input().split(' ')
+        x_test[k][0] = 1.0
         for p in range(m):
-            x_test[k][p] = float(test_line[p])
+            x_test[k][p+1] = float(test_line[p])
 
+    output_y = x_test.dot(b)
+    for y0 in output_y:
+        value = '{:.2f}'.format(y0)
+        print(value)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
